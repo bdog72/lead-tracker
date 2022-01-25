7e34d60df3fb0ec228aa681248d6df4c21e2cb94
Brian Bycynski <high5bri@hotmail.com>
2022-01-25 04:24:21 -0500

    Lead Tracker

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 //
 //
 
-let myLeads = [];
 const inputEl = document.getElementById('input-el');
-
 const inputBtn = document.getElementById('input-btn');
 const ulEl = document.getElementById('ul-el');
+
+let myLeads = [];
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem('leads'));
+console.log(leadsFromLocalStorage);
 
 inputBtn.addEventListener('click', function () {
   myLeads.push(inputEl.value);
   inputEl.value = '';
+  localStorage.setItem('leads', JSON.stringify(myLeads));
   renderLeads();
+  console.log(localStorage.getItem('leads'));
 });
 
 function renderLeads() {
   let listItems = '';
-
   for (let i = 0; i < myLeads.length; i++) {
     listItems += `
     <li class='list-items'>
@@ -24,6 +28,5 @@ function renderLeads() {
       </a>
     </l1>`;
   }
-
   ulEl.innerHTML = listItems;
 }
