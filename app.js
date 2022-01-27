df0e057c446ac9c79a89ba73f22d995a367b088f
Brian Bycynski <high5bri@hotmail.com>
2022-01-27 02:48:07 -0500

    leads tracker

--- app.js
+++ app.js
@@ -5,32 +5,40 @@ let myLeads = [];
 
 const inputEl = document.getElementById('input-el');
 const inputBtn = document.getElementById('input-btn');
+
+const deleteBtn = document.getElementById('delete-btn');
 const ulEl = document.getElementById('ul-el');
 
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem('leads'));
 
 if (leadsFromLocalStorage) {
-  console.log('Leads in LS');
   myLeads = leadsFromLocalStorage;
-  renderLeads();
+  render(myLeads);
 }
 
-inputBtn.addEventListener('click', function () {
-  myLeads.push(inputEl.value);
-  inputEl.value = '';
-  localStorage.setItem('leads', JSON.stringify(myLeads));
-  renderLeads();
-});
-
-function renderLeads() {
+function render(leads) {
   let listItems = '';
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
     <li class='list-items'>
-      <a target="_blank" href="${myLeads[i]}">
-        ${myLeads[i]}
+      <a target="_blank" href="${leads[i]}">
+        ${leads[i]}
       </a>
     </l1>`;
   }
   ulEl.innerHTML = listItems;
 }
+
+deleteBtn.addEventListener('dblclick', function () {
+  console.log(`Double Bozo`);
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
+inputBtn.addEventListener('click', function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = '';
+  localStorage.setItem('leads', JSON.stringify(myLeads));
+  render(myLeads);
+});
